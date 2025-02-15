dd79bb3d6aa0b7cda6e85224bb822eeab02a9220
massimopallera <pallera.massimo@gmail.com>
2025-02-15 09:59:02 +0100

    snack1 done

---
+++ src/org/lessons/java/snacks/Main.java
@@ -0,0 +1,9 @@
+package org.lessons.java.snacks;
+
+public class Main {
+    public static void main(String[] args) {
+        Studente studente = new Studente("Anselmo", "Bitta", 24);
+
+        System.out.println(studente.getData());
+    }
+}
--- src/org/lessons/java/snacks/Studente.java
+++ src/org/lessons/java/snacks/Studente.java
@@ -1,5 +1,16 @@
 package org.lessons.java.snacks;
 
 public class Studente {
-    
+    private String nome, cognome; 
+    private int eta;
+
+    Studente(String nome, String cognome, int eta){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+    }
+
+    public String getData(){
+        return nome + " " + cognome + ", " + eta + " anni";
+    }
 }
